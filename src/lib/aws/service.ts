74cfa5e57c41c2adf563880932ba72220a07acf2
Matthew Barlocker <mbarlocker@gmail.com>
2020-05-30 20:44:57 -0600

    fixup - sorting

--- src/lib/aws/service.ts
+++ src/lib/aws/service.ts
@@ -83,27 +83,27 @@ export abstract class RegionalService<T extends RegionWorker> extends Service<T>
 		super(supportedRegions);
 	}
 
-	get usEast2(): Maybe<T> { return this.getRegion('us-east-2'); }
-	get usEast1(): Maybe<T> { return this.getRegion('us-east-1'); }
-	get usWest1(): Maybe<T> { return this.getRegion('us-west-1'); }
-	get usWest2(): Maybe<T> { return this.getRegion('us-west-2'); }
 	get afSouth1(): Maybe<T> { return this.getRegion('af-south-1'); }
 	get apEast1(): Maybe<T> { return this.getRegion('ap-east-1'); }
-	get apSouth1(): Maybe<T> { return this.getRegion('ap-south-1'); }
-	get apNortheast3(): Maybe<T> { return this.getRegion('ap-northeast-3'); }
+	get apNortheast1(): Maybe<T> { return this.getRegion('ap-northeast-1'); }
 	get apNortheast2(): Maybe<T> { return this.getRegion('ap-northeast-2'); }
+	get apNortheast3(): Maybe<T> { return this.getRegion('ap-northeast-3'); }
+	get apSouth1(): Maybe<T> { return this.getRegion('ap-south-1'); }
 	get apSoutheast1(): Maybe<T> { return this.getRegion('ap-southeast-1'); }
 	get apSoutheast2(): Maybe<T> { return this.getRegion('ap-southeast-2'); }
-	get apNortheast1(): Maybe<T> { return this.getRegion('ap-northeast-1'); }
 	get caCentral1(): Maybe<T> { return this.getRegion('ca-central-1'); }
 	get euCentral1(): Maybe<T> { return this.getRegion('eu-central-1'); }
+	get euNorth1(): Maybe<T> { return this.getRegion('eu-north-1'); }
+	get euSouth1(): Maybe<T> { return this.getRegion('eu-south-1'); }
 	get euWest1(): Maybe<T> { return this.getRegion('eu-west-1'); }
 	get euWest2(): Maybe<T> { return this.getRegion('eu-west-2'); }
-	get euSouth1(): Maybe<T> { return this.getRegion('eu-south-1'); }
 	get euWest3(): Maybe<T> { return this.getRegion('eu-west-3'); }
-	get euNorth1(): Maybe<T> { return this.getRegion('eu-north-1'); }
 	get meSouth1(): Maybe<T> { return this.getRegion('me-south-1'); }
 	get saEast1(): Maybe<T> { return this.getRegion('sa-east-1'); }
+	get usEast1(): Maybe<T> { return this.getRegion('us-east-1'); }
+	get usEast2(): Maybe<T> { return this.getRegion('us-east-2'); }
+	get usWest1(): Maybe<T> { return this.getRegion('us-west-1'); }
+	get usWest2(): Maybe<T> { return this.getRegion('us-west-2'); }
 }
 
 export const Global = 'global';
